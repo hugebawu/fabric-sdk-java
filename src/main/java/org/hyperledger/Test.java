b27a0c96a1ba0979b764eafb0b0914cfe1466d91
hugebawu <drbjhu@163.com>
2020-04-04 18:11:26 +0800

    first commit

---
+++ src/main/java/org/hyperledger/Test.java
@@ -0,0 +1,11 @@
+package org.hyperledger;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.arch"));
+	}
+
+}
